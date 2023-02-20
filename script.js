3b7163362fc2fad7c3b6cb5650c57543d293b990
Jinashree Gane <113277171+Jinny45@users.noreply.github.com>
2023-02-20 13:53:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function calculateMinCost() {
+function minCostFormRope() {
   //your code here
+	a.preventDefault();
+	var inputElement = document.querySelector('input').value;
+	var arr = inputElement.split(',');
+	arr.sort(function(a,b){return a-b});
+     while(arr.length>1){
+		 var res = Number(arr[0]) + Number(arr[1]);
+		 arr.splice(0,2);
+		 aee.push(res);
+		 cost+=res;
+		 arr.sort(function(a,b){return a-b});
+	 }
+	document.getElementById("result").textContent = cost;
   
   
   
