4c27fd274d241d865fa8e6d472a1ceb350f3e71f
Jinashree Gane <113277171+Jinny45@users.noreply.github.com>
2023-03-03 16:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-function minCostToFormRope(a) {
-  a.preventDefault();
-  var inputElement = document.querySelector("input").value;
-  var arr = inputElement.split(",");
-  arr.sort(function (a, b) {
-    return a - b;
-  });
+function calculateMinCost(){
+	let ropelengthsStr=document.getElementById('rope-lengths').value;
+	let ropelengthsArr=ropelengthsStr.split(",").map(Number);
+	let n=ropelengthsArr.length;
 
-  // we need to access first two element
-  // add them & store in a variable res
+let pq=[];
+	for(let i=0;i<n;i++){
+		pq.push(ropelengthsArr[i]);
+	}
+pq.sort((a,b)=>a-b);
 
-  // add the res in array
-  // increment cost by res
-  var cost = 0;
-  while (arr.length > 1) {
-    var res = Number(arr[0]) + Number(arr[1]);
-    arr.splice(0, 2);
-
-    arr.push(res);
-    cost += res;
-
-    arr.sort(function (a, b) {
-      return a - b;
-    });
-  }
-
-  document.getElementById("result").textContent = cost;
+let res=0;
+while(pq.length>1){
+	let first=pq.shift();
+	let second= pq.shift();
+	res+=first+second;
+	pq.push(first+second);
+	pq.sort((a,b)=>a-b);
+}
+	document.getElementById('result').textContent=res;
 }
